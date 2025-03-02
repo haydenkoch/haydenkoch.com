6dc8fcaa05ac656c091ba4aa4e3ff218b0d741a5
Hayden Koch <me@haydenkoch.com>
2025-03-01 20:06:29 -0800

    optimize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <div class="grid h-screen place-items-center">
-    <div class="grid h-screen place-items-center">
-        <h1 class="font-bold text-2xl">
-            Coming Soon<span class="animate-flash">.</span>
-        </h1>
-    </div>
+    <h1 class="font-bold text-2xl">
+        Coming Soon<span class="animate-flash">.</span>
+    </h1>
 </div>
 <a href="mailto:hello@haydenkoch.com" class="absolute bottom-2 left-1/2 -translate-x-1/2 text-center text-xs text-gray-500">
     hello@haydenkoch.com

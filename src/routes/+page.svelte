b9473843efd174092524550ee936c34e24a9f793
Hayden Koch <me@haydenkoch.com>
2025-03-28 15:14:09 -0700

    astro rebuild

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="grid h-dvh place-items-center">
-    <h1 class="font-bold text-2xl">
-        Coming Soon<span class="animate-flash">.</span>
-    </h1>
-</div>
-<a href="mailto:hello@haydenkoch.com" class="absolute bottom-2 left-1/2 -translate-x-1/2 text-center text-xs text-gray-500">
-    hello@haydenkoch.com
-</a>
